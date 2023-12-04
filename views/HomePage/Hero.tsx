e919a50ffc65122cbd95ebe52ac1a7d2e8124b43
booming9 <149972515+booming9@users.noreply.github.com>
2023-12-04 14:02:41 -0300

    Update Hero.tsx

--- views/HomePage/Hero.tsx
+++ views/HomePage/Hero.tsx
@@ -14,11 +14,10 @@ export default function Hero() {
   return (
     <HeroWrapper>
       <Contents>
-        <CustomOverTitle>the coolest, saas product you have ever seen</CustomOverTitle>
-        <Heading>Make your life easier with our SaaS</Heading>
+        <CustomOverTitle>Private Holding</CustomOverTitle>
+        <Heading>Booming 9</Heading>
         <Description>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, tempora qui. Explicabo voluptate iure ipsum molestias
-          repudiandae perspiciatis nostrum praesentium, unde pariatur tempora magni rem. Necessitatibus facilis obcaecati ratione.
+          Consulting Group.
         </Description>
         <CustomButtonGroup>
           <Button onClick={() => setIsModalOpened(true)}>
